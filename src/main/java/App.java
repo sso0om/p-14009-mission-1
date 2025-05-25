372d1f4269786c059aa93fa40df9f8438340a10d
sso0om <ssooom142@gmail.com>
2025-05-25 16:40:54 +0900

    Step4 등록할때 마다 생성되는 명언번호가 증가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
         Scanner sc = new Scanner(System.in);
+        int wiseSayingId = 0;
 
         while (true) {
             System.out.print("명령) ");
@@ -12,13 +13,15 @@ public class App {
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
+                wiseSayingId++;
+
                 System.out.print("명언 : ");
                 sc.nextLine();
 
                 System.out.print("작가 : ");
                 sc.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.%n", wiseSayingId);
             }
         }
         sc.close();
